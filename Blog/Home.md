ac1bb7abc22c39d91f266dd3377572b22a72d85c
joaonunovalente <jnvalente@ua.pt>
2025-09-18 01:22:30 +0100

    Code | Update 'capture_point_clouds.py'

--- Blog/Home.md
+++
@@ -1,5 +0,0 @@
-List of blog posts:
-- [[1. How Surface Angle Affects Laser Vibrometer Measurements]]
-
-
-
--- code/point-cloud-registration/pipeline-multiview-point-cloud-acquisition/exhaustive-grid-search/fft_conv_pytorch/__init__.py
+++
@@ -1,1 +0,0 @@
-from fft_conv_pytorch.fft_conv import FFTConv1d, FFTConv2d, FFTConv3d, fft_conv,WrappedFFTModule
